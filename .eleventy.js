bec084b6e6617fd5ffdb4d8219b6ab6ace2918ef
Peter deHaan <peter@deseloper.com>
2022-05-05 00:47:40 -0700

    Refactor

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addLayoutAlias("base", "layouts/base.njk");
+  eleventyConfig.addLayoutAlias("about", "layouts/about.njk");
+  eleventyConfig.addLayoutAlias("landingpage", "layouts/landingpage.njk");
 
   return {
     dir: {
--- src/en/en.11tydata.js
+++
@@ -1,5 +0,0 @@
-module.exports = () => {
-  return {
-    locale: "en",
-  };
-};
